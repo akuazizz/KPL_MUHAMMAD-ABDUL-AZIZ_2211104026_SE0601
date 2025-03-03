0d6b011adeb19c678a56829064fea877accb0f50
Muhammad Abdul Aziz <muhammadabdulaziz772@gmail.com>
2025-03-03 08:45:20 +0700

    update pertemuan 2

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn = new Button();
+            SuspendLayout();
+            // 
+            // btn
+            // 
+            btn.Location = new Point(349, 290);
+            btn.Name = "btn";
+            btn.Size = new Size(94, 29);
+            btn.TabIndex = 0;
+            btn.Text = "click this";
+            btn.UseVisualStyleBackColor = true;
+            btn.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btn);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btn;
+    }
+}
