42085f8cd8987824f0c0b79a3b4bf15b1e1fab16
Muhammad Abdul Aziz <muhammadabdulaziz772@gmail.com>
2025-03-04 11:52:31 +0700

    Update Tugas Pendahuluan

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/TP/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Implementasi A: Menerima input satu karakter dengan huruf kapital
+        Console.Write("Masukkan satu huruf: ");
+        char huruf = Char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        // Mengecek apakah huruf adalah vokal atau konsonan
+        if (huruf == 'A' || huruf == 'I' || huruf == 'U' || huruf == 'E' || huruf == 'O')
+        {
+            Console.WriteLine($"Huruf {huruf} merupakan huruf vokal");
+        }
+        else
+        {
+            Console.WriteLine($"Huruf {huruf} merupakan huruf konsonan");
+        }
+
+        // Implementasi B: Membuat array bilangan genap dan menampilkannya
+        int[] bilanganGenap = { 2, 4, 6, 8, 10 };
+
+        // Iterasi dan menampilkan output
+        for (int i = 0; i < bilanganGenap.Length; i++)
+        {
+            Console.WriteLine($"Angka genap {i + 1} : {bilanganGenap[i]}");
+        }
+    }
+}
