283c9b167a14eb9526a44fab79950a18345c7b00
Muhammad Abdul Aziz <muhammadabdulaziz772@gmail.com>
2025-03-19 22:42:35 +0700

    menambahkan class data mahasiswa

---
+++ 07_Grammar-Based_Input_Processing_Parsing/TP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul7_2211104026
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa2211104026.ReadJSON();
+        }
+    }
+}
